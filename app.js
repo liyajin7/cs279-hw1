0010c0be4d6ccb0f2be9622d476593d5231432ee
liyajin7 <43145004+liyajin7@users.noreply.github.com>
2022-09-01 19:38:26 -0400

    updated comments

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 // to-do submission
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
     // document.querySelector retrieves reference to the <form> element
-    // addEventListener registers handleSubmitForm event handler function to handle "submit" <form> event
+    // addEventListener registers handleSubmitForm event handler function
+    // to handle "submit" <form> event
 
 // to-do completion / discarding
 document.querySelector('ul').addEventListener('click', handleClickDiscardOrCheck);
@@ -15,14 +16,17 @@ document.getElementById('clearAll').addEventListener('click', handleClearAll);
 
 // EVENT HANDLERS
 function handleSubmitForm(event) {
-    event.preventDefault();                         // makes sure default form submission behavior (of browser) doesn't take place
-    let input = document.querySelector('input');    // retrieves ref to info inputted through form via info
+    event.preventDefault();                         // makes sure default form submission
+                                                    // behavior (of browser) doesn't take place
+    let input = document.querySelector('input');    // retrieves ref to info inputted through
+                                                    // form via info
     if (input.value != '')
         addToDo(input.value);
     input.value = '';                               // resets input's value to empty string
 }
 
-function handleClickDiscardOrCheck(event) {          // runs relevant helper depending on whether click comes from complete or discard button
+function handleClickDiscardOrCheck(event) {          // runs relevant helper depending on whether
+                                                     // click comes from complete or discard button
     if (event.target.name == 'completeButton')
         completeToDo(event);
     if (event.target.name == 'discardButton')
@@ -33,19 +37,26 @@ function handleClickDiscardOrCheck(event) {          // runs relevant helper dep
 // HELPER FUNCS
 function addToDo(toDo) {
     let ul = document.querySelector('ul');          // retrieves ref to the ul element, stored in ul var
-    let li = document.createElement('li');          // creates new li element containing output for each to-do entry, stored in li var
+    let li = document.createElement('li');          // creates new li element containing output for each
+                                                    // to-do entry, stored in li var
     li.innerHTML = `
         <span class="to-do-item">${toDo}</span>
         <button name="completeButton"><i class="fas fa-check-square"></i></button>
         <button name="discardButton" ><i class="fas fa-trash"></i></button> 
-    `;                                              // insert inner HTML code for the li element; insert to-do-item text (input to function) within to-do, which also contains complete & discard buttons
-    // FFD4D4. ; onmouseover="this.style.backgroundColor='#E50000'" onmouseout="this.style.backgroundColor=''"
+    `;                                              // insert inner HTML code for the li element; insert
+                                                    // to-do-item text (input to function) within to-do,
+                                                    // which also contains complete & discard buttons
+                                                    // NOTE: tried using "; onmouseover="this.style.backgroundColor='#E50000'" onmouseout="this.style.backgroundColor=''""
+                                                    // to fix hover states, but didn't work: ended up
+                                                    // (inefficiently) added a new name-specific CSS class
     li.classList.add('to-do-list-item');
-    ul.appendChild(li);                             // new li element added as child of ul element so it becomes visible in browser
+    ul.appendChild(li);                             // new li element added as child of ul element so it
+                                                    // becomes visible in browser
 }
 
 function completeToDo(event) {
-    let item = event.target.parentNode;                      // retrieves ref to parent node AKA list item to apply line-through text style to current item
+    let item = event.target.parentNode;                     // retrieves ref to parent node AKA list item
+                                                            // to apply line-through text style to current item
     if (item.style.textDecoration == 'line-through') {
         item.style.textDecoration = 'none';
         // item.style.color = grey;
@@ -56,7 +67,8 @@ function completeToDo(event) {
 
 function discardToDo(event) {
     let item = event.target.parentNode;
-    item.addEventListener('transitionend', function () {     // registers event handler function for CSS transition event for this node
+    item.addEventListener('transitionend', function () {    // registers event handler function for CSS transition
+                                                            // event for this node
         item.remove();
     } );
 
